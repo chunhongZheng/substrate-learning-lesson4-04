3a38d6857c3644e558dab24f3ddd95d236f51ab1
chunhongZheng <824394795@qq.com>
2022-04-18 16:01:06 +0800

    substrate入门课第四课作业第4题 代码调整优化  20220418

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,25 @@
 ////@author caspar    2022/04/14
 //为枚举交通信号灯实现一个 trait，trait里包含一个返回时间的方法，不同的灯持续的时间不同
-enum traitLight{
+trait TrafficLightState {
+    fn time(state: TrafficLight) -> u16;
+}
+enum TrafficLight{
     Red,
     Green,
     Yellow,
 }
-impl traitLight {
-    fn time(&self)->u16{
-        match &self {
-            traitLight::Red => {
+impl TrafficLightState for TrafficLight {
+    fn time(state: TrafficLight)->u16{
+        match state {
+            TrafficLight::Red => {
                 println!("traitLight Red time is {} second", 30);
                 30
             },
-            traitLight::Green => {
+            TrafficLight::Green => {
                 println!("traitLight Green time is {} second", 40);
                 40
             },
-            traitLight::Yellow => {
+            TrafficLight::Yellow => {
                 println!("traitLight Yellow time is {} second", 10);
                 10
             },
@@ -26,8 +29,12 @@ impl traitLight {
 }
 
 fn main() {
-    let  redLight = traitLight::Red;
-    println!("red light time is {}",redLight.time());
-    let greenLight = traitLight::Green;
-    println!("green light time is {}",greenLight.time());
+    let  redLight = TrafficLight::Red;
+    println!("red light time is {}",TrafficLight::time(redLight));
+    let greenLight = TrafficLight::Green;
+    println!("green light time is {}",TrafficLight::time(greenLight));
+
+
+
+
 }
